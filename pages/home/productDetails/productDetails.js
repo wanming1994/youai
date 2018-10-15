42aa97e25b31ac6fef235bd557eddfd5cdef2353
wanming <978676783@qq.com>
2018-10-15 22:11:00 +0800

    电话号码

--- pages/home/productDetails/productDetails.js
+++ pages/home/productDetails/productDetails.js
@@ -15,18 +15,48 @@ Page(Object.assign({}, swiperAutoHeight, {
     sys: app.globalData.sys, //系统信息
     productData: {}, //数据
     pageLoad: false, //页面加载完成
-
+    hiddenmodalput: true,
+    autoFoucs: false
   },
-
+  //点击按钮痰喘指定的hiddenmodalput弹出框
+  modalinput: function() {
+    this.setData({
+      hiddenmodalput: !this.data.hiddenmodalput,
+      autoFoucs: true
+    })
+  },
+  //取消按钮
+  cancel: function() {
+    this.setData({
+      hiddenmodalput: true,
+      autoFoucs: false
+    });
+  },
+  //确认
+  confirm: function() {
+    this.setData({
+      hiddenmodalput: true,
+      autoFoucs: false
+    })
+  },
+  mobile(e) {
+    this.setData({
+      mobile: e.detail.value
+    })
+  },
   onLoad: function(options) {
-    // if (app.globalData.LOGIN_STATUS) {
-    //   this.getData(options)
-    // } else {
-    //   app.loginOkCallbackList.push(() => {
-    //     this.getData(options)
-    //   })
-    // }
-    this.getData(options)
+    if (app.globalData.LOGIN_STATUS) {
+      this.getData(options)
+    } else {
+      app.loginOkCallbackList.push(() => {
+        this.getData(options)
+      })
+    }
+  },
+  goHome() {
+    wx.switchTab({
+      url: '/pages/home/home',
+    })
   },
   getData(options) {
     let that = this;
